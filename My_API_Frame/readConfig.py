fcb2ed1d241a56ca1bd50c83de44f28d0833f652
sonic64@qq.com <yuhang.gyh@alibaba-inc.com>
2020-04-09 16:15:09 +0800

    new

--- My_API_Frame/readConfig.py
+++ My_API_Frame/readConfig.py
@@ -3,7 +3,7 @@ import configparser
 import getpathInfo
 
 path = getpathInfo.get_Path() #调用实例化
-config_path = os.path.join(path,'config.ini') #这句话是在path路径下再加一级
+config_path = os.path.join(path,'common','config.ini') #这句话是在path路径下再加一级
 config = configparser.ConfigParser() #调用外部的读取配置文件的方法
 config.read(config_path,encoding='utf-8')
 
@@ -18,10 +18,17 @@ class ReadConfig():
 
     def get_mysql(self,name):
         value = config.get('DATABASE',name)
-        return value        
+        return value       
+
+    def get_Url(self,name):
+        scheme = config.get('HTTP','scheme') 
+        url0 = config.get('HTTP','baseurl') 
+        url = scheme + '://' + url0  + ':8888' + '/login' + '?'
+        return url
+
  
 if __name__ == '__main__':# 执行该文件，测试下是否OK
     print('HTTP BaseURL:', ReadConfig().get_http('baidu'))
     print('Email ON_OFF:', ReadConfig().get_email('app'))
     print('DATABSE:'     , ReadConfig().get_mysql('learning'))
-   
+    print('URL:'     , ReadConfig().get_Url(''))
--- My_API_Frame/readExcel.py
+++ My_API_Frame/readExcel.py
@@ -1,4 +1,6 @@
 import os
+import sys  
+sys.path.append('/Users/sonic/Project/Python/My_API_Frame/common')
 import getpathInfo# 自己定义的内部类，该类返回项目的绝对路径
 #调用读Excel的第三方库xlrd
 from xlrd import open_workbook
@@ -9,7 +11,7 @@ class readExcel():
     def get_xls(self, xls_name, sheet_name):# xls_name填写用例的Excel名称 sheet_name该Excel的sheet名称
         cls = []
         # 获取用例文件路径
-        xlsPath = os.path.join(path, "testFile", 'data', xls_name)
+        xlsPath = os.path.join(path, 'data', xls_name)
         file = open_workbook(xlsPath)# 打开用例Excel
         sheet = file.sheet_by_name(sheet_name)#获得打开Excel的sheet
         # 获取这个sheet内容行数
