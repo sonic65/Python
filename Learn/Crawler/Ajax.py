864b61fe7c9994af5729dc8f7d0ee426fa66f904
sonic666 <sonic64@qq.com>
2020-07-22 23:14:46 +0800

    1
    1

---
+++ Learn/Crawler/Ajax.py
@@ -0,0 +1,3 @@
+from urllib.request import Request,urlopen
+from urllib.parse import urlencode
+from fake_useragent import UserAgent
--- Learn/Module/os_open.py
+++ Learn/Module/os_open.py
@@ -1,3 +1,4 @@
 import os
+import math
 
 os.system(r"D:\游戏\TH08_东方永夜抄\th08.exe")
