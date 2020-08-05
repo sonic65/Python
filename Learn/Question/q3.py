5adef13ed9d5d9ebc220cb0b2aaee7602d2e2145
sonic65 <sonic64@qq.com>
2020-08-05 16:09:18 +0800

    1

---
+++ Learn/Question/q3.py
@@ -0,0 +1,7 @@
+x = int(input('Input a number: '))
+d = dict()
+
+for i in range(1, x+1):
+    d[i]=i*i
+
+print(d)
---
+++ Learn/Question/q4.py
@@ -0,0 +1,8 @@
+def fact(x):
+    if x == 0 :
+        return 1
+    return x * fact(x - 1)
+
+x = int(input('Input a number: '))
+
+print(fact(x))
