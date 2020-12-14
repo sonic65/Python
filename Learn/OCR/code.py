fd6d7af80f9ed6288515747bd9cb7210e2ebbd85
“gaoyuhang2” <gaoyuhang2@tal.com>
2020-12-14 19:18:28 +0800

    百度AIP OCR学习

---
+++ Learn/OCR/code.py
@@ -0,0 +1,23 @@
+# 利用aip进行识别
+
+APP_ID = '20545078'
+API_KEY = 'qROAdLBMg7msxQgCB5FBwXIc'
+SECRECT_KEY = '2KEtyNrmIstLt48hzMvnAXaNMQKg88a6'
+
+def identify_words(index=0):
+    from aip import AipOcr
+    client = AipOcr(APP_ID, API_KEY, SECRECT_KEY)
+    with open(r'code.png', 'rb') as f:
+        img = f.read()
+    message = client.basicGeneral(img)
+    res = message['words_result']
+    print(res)
+    print('识别结果为: %s' % res[0]['words'])
+    if len(res) == 0:
+        return '1'
+    else:
+        return res[index]['words']
+
+
+if __name__ == '__main__':
+    identify_words()
